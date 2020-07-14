90263691d081b3401a257b8f6928e942aa9e0930
jjy <zbxm2008@naver.com>
2020-07-14 09:24:04 +0900

    testExam 추가

---
+++ 0714/src/TestExam.java
@@ -0,0 +1,17 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=5; i++) {
+			System.out.print(i+"  ");
+		}
+		System.out.println();
+		for(int j=10; j>=6; j--) {
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+
+}
